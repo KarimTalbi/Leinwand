077c2df0597cedb2ae5363eaed518c561da8e018
KarimTalbi <karim.talbi7@gmail.com>
2026-04-06 21:12:07 +0200

    refactor `Flow`: clean up imports, simplify Sync Indicator and dropdown code formatting, and add spacing adjustments for improved readability

--- frontend/src/Flow.tsx
+++ frontend/src/Flow.tsx
@@ -2,9 +2,7 @@ import {useEffect} from "react";
 import {useShallow} from 'zustand/react/shallow'
 import {ReactFlow, Background, Controls, NodeTypes, Panel, useReactFlow, MiniMap} from '@xyflow/react';
 import {Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/react';
-import {
-    ChevronDownIcon, PlusIcon
-} from '@heroicons/react/16/solid'
+import {ChevronDownIcon, PlusIcon} from '@heroicons/react/16/solid'
 
 import PromptNode from './nodetypes/promptnode.tsx';
 import TextNode from './nodetypes/textnode.tsx';
@@ -95,13 +93,13 @@ function Flow() {
         <div className="relative h-screen w-screen overflow-hidden">
 
             {/* Sync Indicator */}
-            <div
-                className="absolute top-4 right-4 z-50 flex items-center gap-2 rounded-full bg-white backdrop-blur-md px-3 py-1 border border-white/20 shadow-lg">
-                <div
-                    className={`h-2.5 w-2.5 rounded-full ${syncing ? 'animate-pulse bg-yellow-400' : 'bg-green-500'}`}/>
+            <div className="absolute top-4 right-4 z-50 flex items-center gap-2 rounded-full bg-white backdrop-blur-md px-3 py-1 border border-white/20 shadow-lg">
+
+                <div className={`h-2.5 w-2.5 rounded-full ${syncing ? 'animate-pulse bg-yellow-400' : 'bg-green-500'}`}/>
                 <span className="text-s font-medium text-black/80">
                     {syncing ? 'Syncing...' : 'Saved'}
                 </span>
+
             </div>
 
             {/* React Flow */}
@@ -120,9 +118,13 @@ function Flow() {
                 maxZoom={2}
             >
                 <Background bgColor="#ebebeb" size={2} gap={20} color="#CCCCCC"/>
+
                 <Controls/>
+
                 <Panel position="top-left" className="flex gap-2">
+
                     <Menu>
+
                         <MenuButton className="inline-flex items-center gap-2 rounded-md bg-white px-3 py-1.5 text-sm/6 font-semibold text-gray-600  shadow-xl focus:not-data-focus:outline-none data-focus:outline data-focus:outline-gray-600 data-hover:bg-gray-100 data-open:bg-gray-100">
                             Add Node
                             <ChevronDownIcon className="size-4 fill-gray-600" />
@@ -133,30 +135,40 @@ function Flow() {
                             anchor='bottom start'
                             className="w-40 origin-top-right rounded-xl border border-white/5 bg-white p-1 text-sm/6 text-black font-bold transition duration-100 ease-out [--anchor-gap:--spacing(1)] focus:outline-none data-closed:scale-95 data-closed:opacity-0"
                         >
+
                             <MenuItem>
                                 <button onClick={onCreatePromptNode} className="group flex w-full items-center justify-between rounded-lg px-3 py-1.5 data-focus:bg-black/10">
                                     Prompt Node
                                     <PlusIcon className="size-4 fill-black/40" />
                                 </button>
                             </MenuItem>
+
                             <div className="my-0 h-px bg-black/5" />
+
                             <MenuItem>
                                 <button onClick={onCreateTextNode} className="group flex w-full items-center justify-between rounded-lg px-3 py-1.5 data-focus:bg-black/10">
                                     Text Node
                                     <PlusIcon className="size-4 fill-black/40" />
                                 </button>
                             </MenuItem>
+
                             <div className="my-0 h-px bg-black/5" />
+
                             <MenuItem>
                                 <button onClick={onCreateMergeNode} className="group flex w-full items-center justify-between rounded-lg px-3 py-1.5 data-focus:bg-black/10">
                                     Merge Node
                                     <PlusIcon className="size-4 fill-black/40" />
                                 </button>
                             </MenuItem>
+
                         </MenuItems>
+
                     </Menu>
+
                 </Panel>
+
                 <MiniMap nodeColor="gray"/>
+
             </ReactFlow>
         </div>
     );
