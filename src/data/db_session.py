14d86167abc9a41a5204a266072df608f6ff086b
KarimTalbi <karim.talbi7@gmail.com>
2026-04-10 23:00:10 +0200

    Added detailed docstrings to `get_async_session` and `init_db` in `db_session.py` to improve code clarity and documentation.

--- src/data/db_session.py
+++ src/data/db_session.py
@@ -33,12 +33,37 @@ async_session: async_sessionmaker[AsyncSession] = async_sessionmaker(
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Creates and manages an asynchronous session generator for database interactions.
+
+    This method provides an `AsyncSession` instance that is created using the
+    `async_session` factory. It ensures that the session is properly initialized
+    and closed after usage, adhering to proper session management practices in an
+    asynchronous context.
+
+    Yields:
+        AsyncGenerator[AsyncSession, None]: An asynchronous generator that provides
+        an active database session for performing operations.
+
+    """
     async with async_session() as session:
         async with session.begin():
             yield session
 
 
 async def init_db(reset: bool = False):
+    """
+    Initialize the database and optionally reset its schema.
+
+    This function establishes a connection with the database using an asynchronous
+    engine. If the reset parameter is set to True, it will drop all existing
+    database tables before recreating them. Otherwise, it simply ensures that the
+    database schema exists and is created.
+
+    Args:
+        reset (bool): If True, the database tables will be dropped before being
+            recreated. Defaults to False.
+    """
     async with engine.begin() as conn:
         if reset:
             await conn.run_sync(Base.metadata.drop_all)
