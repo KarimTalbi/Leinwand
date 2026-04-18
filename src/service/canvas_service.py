761a2a4e7dcd16cb07e73773219fe34399aae8e9
KarimTalbi <karim.talbi7@gmail.com>
2026-04-18 16:49:26 +0200

    refactor: update canvas service methods to include `user_id` and `canvas_id` for improved data context and alignment with user filtering

--- src/service/canvas_service.py
+++ src/service/canvas_service.py
@@ -9,7 +9,7 @@ from src.data.db_models import Canvas
 from utils.exceptions import CanvasNotFoundException
 
 
-async def get_canvas(session: AsyncSession, canvas_id: UUID,  user_id: UUID) -> Canvas:
+async def get_canvas(session: AsyncSession, canvas_id: UUID, user_id: UUID) -> Canvas:
     result = await session.execute(
         select(Canvas).where(Canvas.id == canvas_id, Canvas.user_id == user_id)
     )
@@ -21,8 +21,10 @@ async def get_canvas(session: AsyncSession, canvas_id: UUID,  user_id: UUID) -> 
     return canvas
 
 
-async def create_canvas(session: AsyncSession, canvas: CanvasCreate) -> Canvas:
-    new_canvas = Canvas(**canvas.model_dump())
+async def create_canvas(
+    session: AsyncSession, canvas: CanvasCreate, user_id: UUID
+) -> Canvas:
+    new_canvas = Canvas(**canvas.model_dump(), user_id=user_id)
     session.add(new_canvas)
 
     await session.flush()
@@ -36,8 +38,10 @@ async def list_canvases(session: AsyncSession, user_id: UUID) -> Sequence[Canvas
     return result.scalars().all()
 
 
-async def update_canvas(session: AsyncSession, canvas_update: CanvasUpdate, user_id: UUID) -> Canvas:
-    canvas = await get_canvas(session, canvas_update.id, user_id)
+async def update_canvas(
+    session: AsyncSession, canvas_update: CanvasUpdate, canvas_id: UUID, user_id: UUID
+) -> Canvas:
+    canvas = await get_canvas(session, canvas_id, user_id)
 
     for key, value in canvas_update.model_dump(exclude_unset=True).items():
         setattr(canvas, key, value)
